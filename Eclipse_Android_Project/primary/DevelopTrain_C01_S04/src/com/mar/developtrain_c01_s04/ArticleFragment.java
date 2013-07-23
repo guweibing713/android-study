5a7596496c85166ddd67784d73b6369dcf72beb1
mar.belrare <guweibing713@hotmail.com>
2013-07-23 13:56:06 +0800

    add ArticleFragment and HeadlinesFragment files

---
+++ Eclipse_Android_Project/primary/DevelopTrain_C01_S04/src/com/mar/developtrain_c01_s04/ArticleFragment.java
@@ -0,0 +1,20 @@
+package com.mar.developtrain_c01_s04;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ArticleFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	
+
+}
