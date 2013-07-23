ea282c2dc6827f85d7335172bdc19ff13203c8b7
mar.belrare <guweibing713@hotmail.com>
2013-07-23 18:03:03 +0800

    modify some files and debug with physical device, but it does work well, it's crashed when click the dail button.

--- Eclipse_Android_Project/primary/DevelopTrain_C01_S06/src/com/mar/developtrain_c01_s06/MainActivity.java
+++ Eclipse_Android_Project/primary/DevelopTrain_C01_S06/src/com/mar/developtrain_c01_s06/MainActivity.java
@@ -1,14 +1,22 @@
 package com.mar.developtrain_c01_s06;
 
+import java.util.List;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		System.out.println("Activity start...");
 		setContentView(R.layout.activity_main);
 	}
 
@@ -17,6 +25,25 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void ClickButton(){
+		
+		//EditText editText = (EditText) findViewById(R.id.editText);
+		//String str = "tel" + editText.toString();
+		System.out.println("Tel is :");
+		Uri number = Uri.parse("tel:10010");
+		Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+		
+		// Verify it resolves
+		PackageManager packageManager = getPackageManager();
+		List<ResolveInfo> activities = packageManager.queryIntentActivities(callIntent, 0);
+		boolean isIntentSafe = activities.size() > 0;
+		  
+		// Start an activity if it's safe
+		if (isIntentSafe) {
+		    startActivity(callIntent);
+		}
 	}
 
 }
