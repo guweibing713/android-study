e621fbf0b0ace5d11f56d9c30ca048b20412323e
mar.belrare <guweibing713@hotmail.com>
2013-07-23 12:48:27 +0800

    SaveData and GetData Examlpe

--- Eclipse_Android_Project/primary/DevelopTrain_C01_S05/src/com/mar/developtrain_c01_s05/MainActivity.java
+++ Eclipse_Android_Project/primary/DevelopTrain_C01_S05/src/com/mar/developtrain_c01_s05/MainActivity.java
@@ -2,7 +2,10 @@ package com.mar.developtrain_c01_s05;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -19,4 +22,35 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void Save(){
+		
+		Context context = getApplicationContext();
+		SharedPreferences sharedPref = context.getSharedPreferences(
+		        getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+		
+		EditText editText = (EditText) findViewById(R.id.edittext_login);	
+		
+		String userName = editText.getText().toString();
+		SharedPreferences.Editor editor = sharedPref.edit();
+		editor.putString(getString(R.string.saveName), userName);
+		editor.commit();
+	}
+	
+	
+	public void GetRecord(){
+		
+		Context context = getApplicationContext();
+		
+		SharedPreferences sharedPref = context.getSharedPreferences(
+		        getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+		
+		String defaultValue = getResources().getString(R.string.saveName);
+		String saveName = sharedPref.getString(getString(R.string.saveName), defaultValue);
+		
+		EditText editText = (EditText) findViewById(R.id.textview_display);	
+		
+		editText.setText(saveName);
+		
+	}
+	
 }
